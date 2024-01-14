c8abf8aadf954738771fb16828e867a746cd5412
Ridwan Adebosin <adebosinadewale@gmail.com>
2024-01-14 21:22:08 +0100

    StepMessage component added

--- src/App.js
+++ src/App.js
@@ -48,9 +48,7 @@ function Steps() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <p className="message">
-            Step{step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
           <div className="buttons">
             <Button bgColor="#7950f2" onClick={handlePrevious} textColor="#fff">
@@ -64,6 +62,17 @@ function Steps() {
         </div>
       )}
     </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <>
+      <p className="message">
+        <h3>Step{step}</h3> {children}
+      </p>
+      ;
+    </>
   );
 }
 

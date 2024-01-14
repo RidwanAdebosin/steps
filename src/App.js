6e524431a4800f415eb6e2f82dac15e6eb397465
Ridwan Adebosin <adebosinadewale@gmail.com>
2024-01-14 18:35:15 +0100

    reusable button component added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ export default function App() {
   return (
     <div>
       <Steps />
-      <Steps />
     </div>
   );
 }
@@ -54,21 +53,27 @@ function Steps() {
           </p>
 
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button bgColor="#7950f2" onClick={handlePrevious} textColor="#fff">
+              <span>👈</span>Previous
+            </Button>
+
+            <Button bgColor="#7950f2" onClick={handleNext} textColor="#fff">
+              <span>👉</span>Next
+            </Button>
           </div>
         </div>
       )}
     </div>
+  );
+}
+
+function Button({ textColor, onClick, bgColor, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
